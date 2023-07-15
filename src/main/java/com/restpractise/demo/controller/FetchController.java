7dd743ca7697ab541048343666008bf5240e5660
garvit <garv8tverma26@gmail.com>
2023-07-15 14:04:33 +0530

    Completed FetchNextNumber API endpoint

--- src/main/java/com/restpractise/demo/controller/FetchController.java
+++ src/main/java/com/restpractise/demo/controller/FetchController.java
@@ -1,21 +1,52 @@
 package com.restpractise.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.restpractise.demo.entity.DataObject;
+import com.restpractise.demo.repository.DataObjectRepository;
+import com.restpractise.demo.service.Calculations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/api")
 public class FetchController {
+    private final DataObjectRepository objRepo;
 
-    @GetMapping("/FetchNextNumber")
-    public Map<String, Integer> FetchNextNumber() {
-        Map<String, Integer> dataMap = new HashMap<>();
+    @Autowired
+    private Calculations calculations;
 
-        return dataMap;
+    @Autowired
+    private DataObjectController doc;
+
+    public FetchController(DataObjectRepository objRepo) {
+        this.objRepo = objRepo;
+    }
+
+    @GetMapping("/FetchNextNumber/{categoryCode}")
+    public Map<String, Integer> FetchNextNumber(@PathVariable String categoryCode) {
+        Map<String, Integer> result = new HashMap<>();
+        DataObject obj = doc.getObject(categoryCode);
+
+        int oldValue = obj.getValue();
+        result.put("oldValue", oldValue);
+        result.put("newValue", calculations.calculate(oldValue));
+
+//        Optional<DataObject> obj = objRepo.findById(categoryCode);
+//        if(obj.isPresent()) {
+//            int oldValue = obj.get().getValue();
+//            result.put("oldValue", oldValue);
+//            System.out.println("In function!" + result);
+//            Calculations calculations = new Calculations();
+//            result.put("newValue", calculations.calculate(oldValue));
+//
+//        } else {
+//            result.put("oldValue", 0);
+//            result.put("newValue", 0);
+//        }
+        DataObject newObj = new DataObject(categoryCode, result.get("newValue"));
+        doc.updateObject(newObj);
+        return result;
     }
 
 }
