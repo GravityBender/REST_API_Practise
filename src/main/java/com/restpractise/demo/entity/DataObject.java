eeb3bbe8db6f8a718a95844fbbdab6e4484f1c40
garvit <garv8tverma26@gmail.com>
2023-07-15 14:03:23 +0530

    Modified the DataObject Entity

--- src/main/java/com/restpractise/demo/entity/DataObject.java
+++ src/main/java/com/restpractise/demo/entity/DataObject.java
@@ -1,8 +1,29 @@
 package com.restpractise.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rest_practise")
 public class DataObject {
+
+    @Id
+    @Column(name = "categorycode")
     private String categoryCode;
+
+    @Column(name = "value")
     private Integer value;
+
+    public DataObject() {
+
+    }
+
+    public DataObject(String categoryCode, Integer value) {
+        this.categoryCode = categoryCode;
+        this.value = value;
+    }
 
     public String getCategoryCode() {
         return categoryCode;
